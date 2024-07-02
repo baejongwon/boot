f150b4fc703ec50b8d735220840acb510fc528a6
jongwon <jongwon@DESKTOP-COV8KMR>
2024-07-02 21:59:53 +0900

    aa

--- boot/src/main/java/com/care/boot/BootApplication.java
+++ boot/src/main/java/com/care/boot/BootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class BootApplication{
+public class BootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
